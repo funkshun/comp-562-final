b98dd3d1ca1bf006a6514f858e23e6c23d7fec9a
Pyrrxa <pyrrxa@gmail.com>
2020-05-05 20:09:12 -0400

    Updated Report

---
+++ report/auto/report.el
@@ -0,0 +1,25 @@
+(TeX-add-style-hook
+ "report"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("babel" "english")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "graphicx"
+    "subcaption"
+    "multicol"
+    "inputenc"
+    "babel")
+   (LaTeX-add-labels
+    "fig:sfig1"
+    "fig:sfig2"
+    "fig:sfig3"
+    "fig:sfig4"
+    "fig:sfig5"
+    "fig:sfig6"
+    "fig:fig1"
+    "table:1"))
+ :latex)
+
